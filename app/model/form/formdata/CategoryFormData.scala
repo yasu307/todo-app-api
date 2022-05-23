90413ff2e87f7c3f0f96691327eada4a59800f9b
Yusuke Yasuoka <yusuke.yasuoka@nextbeat.net>
2022-05-23 10:09:18 +0900

    add alphabet and numeric constraint to slug form

--- app/model/form/formdata/CategoryFormData.scala
+++ app/model/form/formdata/CategoryFormData.scala
@@ -3,6 +3,7 @@ package model.form.formdata
 import lib.model.Category
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.validation.Constraints
 
 // カテゴリ追加画面のフォームで使用するデータ
 case class CategoryFormData(
@@ -16,8 +17,9 @@ object CategoryFormData {
   val form = Form(
     mapping(
       "name"  -> nonEmptyText(maxLength = 255),
-      "slug"  -> nonEmptyText(maxLength = 64),
-      "color" -> longNumber.transform[Category.Color](l => Category.Color(l.toShort), _.code)
+      // MySQLに英数字のみ許可する制約があるが、formでもチェックしないとエラー画面に移動してしまうので追加
+      "slug"  -> nonEmptyText(maxLength = 64).verifying(Constraints.pattern("""[ -~]+""".r, error = "英数字で入力してください")),
+      "color" -> longNumber.transform[Category.Color](l => Category.Color(l.toShort), _.code),
     )(CategoryFormData.apply)(CategoryFormData.unapply)
   )
 }
